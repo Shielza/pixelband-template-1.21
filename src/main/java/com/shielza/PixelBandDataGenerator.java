da7e0c092d427a3b985c9c86a590dbc2bd7afa59
ImRes <2655678573@qq.com>
2026-04-02 23:01:46 +0800

    Initial commit

---
+++ src/main/java/com/shielza/PixelBandDataGenerator.java
@@ -0,0 +1,11 @@
+package com.shielza;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PixelBandDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
